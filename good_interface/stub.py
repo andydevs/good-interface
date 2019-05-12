47e2a03e46c67b01b30ae3c4fe90873809bcc498
andydevs <akanshul97@gmail.com>
2019-05-11 21:38:28 -0400

    Implemented stub class

--- good_interface/stub.py
+++ good_interface/stub.py
@@ -9,9 +9,39 @@ classes and objects.
 Author:  Anshul Kharbanda
 Created: 6 - 26 - 2018
 """
+from inspect import getfullargspec
 
 class stub:
     """
     Method stub
     """
-    pass
+    def __init__(self, required, varargs=False, kwargs=False, defimpl=None):
+        """
+        Initializes stub
+
+        :param required: Number of required arguments
+        :param varargs:  True if varargs are allowed
+        :param kwargs:   True if kwargs are allowed
+        :param defimpl:  Default implementation of function
+        """
+        self.required = required
+        self.varargs  = varargs
+        self.kwargs   = kwargs
+        self.defimpl  = defimpl
+
+    def valid(self, func):
+        """
+        True if the given function is valid according to stub
+
+        :param func: function to check
+
+        :return: True if the given function is valid
+        """
+        aspec = getfullargspec(func)
+        nargs = len(aspec.args) - len(aspec.defaults or [])
+        varargs = aspec.varargs is not None
+        kwargs = aspec.varkw is not None or aspec.defaults is not None
+        return all([
+            self.required == nargs,
+            self.varargs == varargs,
+            self.kwargs == kwargs])

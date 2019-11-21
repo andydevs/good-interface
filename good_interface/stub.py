1b1ba469b1386b66b3980ec5a59e9ad6dc48a4ba
andydevs <akanshul97@gmail.com>
2019-11-21 18:30:22 -0500

    Created get_stubs and make_stubs_from_func to be used in new interface system

--- good_interface/stub.py
+++ good_interface/stub.py
@@ -14,19 +14,21 @@ from inspect import getfullargspec
 
 class Stub:
     """
-    Method stub object. Defines input/output behavior of method
+    Function stub object. Defines input/output behavior of function
     """
-    def __init__(self, nargs, varargs=False, kwargs=False):
+    def __init__(self, nargs, varargs=False, kwargs=False, method=False):
         """
         Initialize Stub class
 
         :param nargs:   Number of required input arguments
         :param varargs: True if varargs is applied
         :param kwargs:  True if kwargs is applied
+        :param method:  True if spec is for method
         """
         self._nargs = nargs
         self._varargs = varargs
         self._kwargs = kwargs
+        self._method = method
 
     def check(self, func):
         """
@@ -37,6 +39,37 @@ class Stub:
         :return: True if the given function implements the stub
         """
         argspec = getfullargspec(func)
-        return len(argspec.args) == self._nargs \
+        return (len(argspec.args) - (1 if self._method else 0) == self._nargs) \
             and (bool(argspec.varargs) == bool(self._varargs)) \
             and (bool(argspec.varkw) == bool(self._kwargs))
+
+
+def make_stub_from_func(func, method=False):
+    """
+    Create stub from function
+
+    :param func: function to create stub from
+    :param method: true if the function is a method
+
+    :return: stub created from function
+    """
+    argspec = getfullargspec(func)
+    return Stub(
+        len(argspec.args) - (1 if method else 0),
+        bool(argspec.varargs),
+        bool(argspec.varkw),
+        method)
+
+
+def get_stubs(ifcs):
+    """
+    Return stubs of class skeleton
+
+    :param ifcs: interface class skeleton
+
+    :return: Dictionary of stubs in class
+    """
+    return {
+        name: Stub(stub._nargs, stub._varargs, stub._kwargs, True) if isinstance(stub, Stub) else make_stub_from_func(stub, True)
+        for name, stub in ifcs.__dict__.items()
+        if isinstance(stub, Stub) or callable(stub)}
--- test/stub_test.py
+++ test/stub_test.py
@@ -13,40 +13,141 @@ import unittest
 import good_interface.stub
 
 
+# Test Functions
+
+
+def test_func_1(a, b, c):
+    pass
+
+
+def test_func_2(a, b):
+    pass
+
+
+def test_func_3(a, b, *c):
+    pass
+
+
+def test_func_4(a, *b, **c):
+    pass
+
+
+class TestClass:
+    def test_meth_1(self, a, b):
+        pass
+
+    def test_meth_2(self, a, b, *c):
+        pass
+
+    def test_meth_3(self, a, b, *c, **d):
+        pass
+
+
+class TestInterface:
+    meth1 = good_interface.stub.Stub(4, varargs=True)
+
+    def meth2(self, a, b, c, *d, **e):
+        pass
+
+    def meth3(self, a, b):
+        pass
+
+
 class StubTest(unittest.TestCase):
-    def test_check(self):
+    def setUp(self):
         # Test stubs
-        stub1 = good_interface.stub.Stub(3)
-        stub2 = good_interface.stub.Stub(2)
-        stub3 = good_interface.stub.Stub(2, varargs=True)
-        stub4 = good_interface.stub.Stub(1, varargs=True, kwargs=True)
+        self.stub1  = good_interface.stub.Stub(3)
+        self.stub2  = good_interface.stub.Stub(2)
+        self.stub3  = good_interface.stub.Stub(2, varargs=True)
+        self.stub4  = good_interface.stub.Stub(1, varargs=True, kwargs=True)
+        self.stubm1 = good_interface.stub.Stub(2, method=True)
+        self.stubm2 = good_interface.stub.Stub(2, varargs=True, method=True)
+        self.stubm3 = good_interface.stub.Stub(2, varargs=True, kwargs=True, method=True)
 
-        # Test Functions
-        def testFunc1(a, b, c): pass
-        def testFunc2(a, b): pass
-        def testFunc3(a, b, *c): pass
-        def testFunc4(a, *b, **c): pass
-
+    def test_stub_check(self):
         # Test for function 1
-        self.assertTrue(stub1.check(testFunc1))
-        self.assertFalse(stub2.check(testFunc1))
-        self.assertFalse(stub3.check(testFunc1))
-        self.assertFalse(stub4.check(testFunc1))
+        self.assertTrue(self.stub1.check(test_func_1))
+        self.assertFalse(self.stub2.check(test_func_1))
+        self.assertFalse(self.stub3.check(test_func_1))
+        self.assertFalse(self.stub4.check(test_func_1))
 
         # Test for function 2
-        self.assertFalse(stub1.check(testFunc2))
-        self.assertTrue(stub2.check(testFunc2))
-        self.assertFalse(stub3.check(testFunc2))
-        self.assertFalse(stub4.check(testFunc2))
+        self.assertFalse(self.stub1.check(test_func_2))
+        self.assertTrue(self.stub2.check(test_func_2))
+        self.assertFalse(self.stub3.check(test_func_2))
+        self.assertFalse(self.stub4.check(test_func_2))
 
         # Test for function 3
-        self.assertFalse(stub1.check(testFunc3))
-        self.assertFalse(stub2.check(testFunc3))
-        self.assertTrue(stub3.check(testFunc3))
-        self.assertFalse(stub4.check(testFunc3))
+        self.assertFalse(self.stub1.check(test_func_3))
+        self.assertFalse(self.stub2.check(test_func_3))
+        self.assertTrue(self.stub3.check(test_func_3))
+        self.assertFalse(self.stub4.check(test_func_3))
 
         # Test for function 4
-        self.assertFalse(stub1.check(testFunc4))
-        self.assertFalse(stub2.check(testFunc4))
-        self.assertFalse(stub3.check(testFunc4))
-        self.assertTrue(stub4.check(testFunc4))
+        self.assertFalse(self.stub1.check(test_func_4))
+        self.assertFalse(self.stub2.check(test_func_4))
+        self.assertFalse(self.stub3.check(test_func_4))
+        self.assertTrue(self.stub4.check(test_func_4))
+
+    def test_stub_check_meth(self):
+        # Test for method 1
+        self.assertTrue(self.stubm1.check(TestClass.test_meth_1))
+        self.assertFalse(self.stubm2.check(TestClass.test_meth_1))
+        self.assertFalse(self.stubm3.check(TestClass.test_meth_1))
+
+        # Test for method 2
+        self.assertFalse(self.stubm1.check(TestClass.test_meth_2))
+        self.assertTrue(self.stubm2.check(TestClass.test_meth_2))
+        self.assertFalse(self.stubm3.check(TestClass.test_meth_2))
+
+        # Test for method 3
+        self.assertFalse(self.stubm1.check(TestClass.test_meth_3))
+        self.assertFalse(self.stubm2.check(TestClass.test_meth_3))
+        self.assertTrue(self.stubm3.check(TestClass.test_meth_3))
+
+    def test_make_stub_from_func(self):
+        # Test for function 1
+        stub = good_interface.stub.make_stub_from_func(test_func_1)
+        self.assertEqual(stub._nargs, self.stub1._nargs)
+        self.assertEqual(stub._varargs, self.stub1._varargs)
+        self.assertEqual(stub._kwargs, self.stub1._kwargs)
+
+        # Test for function 2
+        stub = good_interface.stub.make_stub_from_func(test_func_2)
+        self.assertEqual(stub._nargs, self.stub2._nargs)
+        self.assertEqual(stub._varargs, self.stub2._varargs)
+        self.assertEqual(stub._kwargs, self.stub2._kwargs)
+
+        # Test for function 3
+        stub = good_interface.stub.make_stub_from_func(test_func_3)
+        self.assertEqual(stub._nargs, self.stub3._nargs)
+        self.assertEqual(stub._varargs, self.stub3._varargs)
+        self.assertEqual(stub._kwargs, self.stub3._kwargs)
+
+        # Test for function 4
+        stub = good_interface.stub.make_stub_from_func(test_func_4)
+        self.assertEqual(stub._nargs, self.stub4._nargs)
+        self.assertEqual(stub._varargs, self.stub4._varargs)
+        self.assertEqual(stub._kwargs, self.stub4._kwargs)
+
+    def test_get_stubs(self):
+        stub1 = good_interface.stub.Stub(4, varargs=True, method=True)
+        stub2 = good_interface.stub.Stub(3, varargs=True, kwargs=True, method=True)
+        stub3 = good_interface.stub.Stub(2, method=True)
+
+        stubs = good_interface.stub.get_stubs(TestInterface)
+
+        self.assertIn('meth1', stubs)
+        self.assertEqual(stub1._nargs, stubs['meth1']._nargs)
+        self.assertEqual(stub1._varargs, stubs['meth1']._varargs)
+        self.assertEqual(stub1._kwargs, stubs['meth1']._kwargs)
+
+        self.assertIn('meth2', stubs)
+        self.assertEqual(stub2._nargs, stubs['meth2']._nargs)
+        self.assertEqual(stub2._varargs, stubs['meth2']._varargs)
+        self.assertEqual(stub2._kwargs, stubs['meth2']._kwargs)
+
+        self.assertIn('meth3', stubs)
+        self.assertEqual(stub3._nargs, stubs['meth3']._nargs)
+        self.assertEqual(stub3._varargs, stubs['meth3']._varargs)
+        self.assertEqual(stub3._kwargs, stubs['meth3']._kwargs)
